0b448da0471c6cbab850afb3ff48ac6f8b0f1275
Martyna Gudwilowicz <m.gudwilowicz@gmail.com>
2025-11-19 10:48:44 +0100

    Add filter_by_age function and not-found message

--- filter_users.py
+++ filter_users.py
@@ -5,17 +5,40 @@ def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
 
+    found = False
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
     for user in filtered_users:
         print(user)
+        found = True
+
+    if not found:
+        print("User with this age not found.")
+
+
+def filter_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    found = False
+    filtered_users= [user for user in users if user["age"] == age]
+
+    for user in filtered_users:
+        print(user)
+        found = True
+
+    if not found:
+        print("User with this name not found.")
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Please enter  'name' or 'age': ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = int(input("Enter an age to filter users: "))
+        filter_by_age(age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
