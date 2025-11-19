8fc8a7bb1317f1821015bc417fd9aada6f675533
mgudwilowicz <m.gudwilowicz@gmail.com>
2025-11-19 16:49:57 +0100

    style: refactor code to follow PEP 8

--- filter_users.py
+++ filter_users.py
@@ -2,6 +2,9 @@ import json
 
 
 def filter_users_by_name(name):
+    """
+        Filters and prints users whose name matches the given value.
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -17,6 +20,9 @@ def filter_users_by_name(name):
 
 
 def filter_by_age(age):
+    """
+        Filters and prints users whose age matches the given value.
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -30,7 +36,11 @@ def filter_by_age(age):
     if not found:
         print("User with this age not found.")
 
+
 def filter_by_email(email):
+    """
+         Filters and prints users whose email matches the given value.
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -46,6 +56,13 @@ def filter_by_email(email):
 
 
 if __name__ == "__main__":
+    """
+     Entry point of the program.
+
+     Prompts the user to choose a filter option (name, age, or email) and 
+     calls the corresponding filtering function. If the user enters an 
+     unsupported option, a message is displayed.
+     """
     filter_option = input("What would you like to filter by? (Please enter  'name', 'age' or 'email': ").strip().lower()
 
     if filter_option == "name":
@@ -55,7 +72,7 @@ if __name__ == "__main__":
         age_to_search = int(input("Enter an age to filter users: "))
         filter_by_age(age_to_search)
     elif filter_option == "email":
-        email_to_search = input("Enter a email to filter users: ").strip()
+        email_to_search = input("Enter the full email address (e.g., user@example.com): ").strip().lower()
         filter_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
